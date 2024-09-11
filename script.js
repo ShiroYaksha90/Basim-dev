815909d0fe8f42058f7b5d7df4591bd0e2bfe8cb
ShiroYaksha90 <aljazaeribasim@gmail.com>
2024-09-11 03:37:21 +0300

    Add a dynamic generated popup modals for projects cards

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const projectsCards = [
     cardDesc:
       "This project is about building a mobile web application to compare metrics from different countries. This project utilizes an API to retrieve information about countries and continents, which is then displayed through the use of React functions.",
     cardStack: ["React", "Redux", "CSS", "HTML"],
-    cardImage: "assets/main-project.PNG",
+    cardImage: "assets/my-world.PNG",
     cardbackgroundstyle:
       "background:url(./assets/my-world.PNG);background-size: 100% 100%;background-repeat: no-repeat;",
     liveLink: "https://my-world-sems.onrender.com/",
@@ -89,12 +89,51 @@ for (let i = 0; i < projectsCards.length; i++) {
             ${stackString}
             </ul>
           </div>
-          <button class="border-0 p-3" data-bs-toggle="modal" data-bs-target="#exampleModal" >See Project</button>`;
+          <button class="border-0 p-3" data-bs-toggle="modal" data-bs-target="#card-${projectsCards[i].cardId}" >See Project</button>`;
 }
 
-const myModal = document.getElementById("myModal");
-const myInput = document.getElementById("myInput");
-
-myModal.addEventListener("shown.bs.modal", () => {
-  myInput.focus();
+const cardModal = document.querySelector(".card-modal");
+cardModal.innerHTML = "";
+projectsCards.forEach((card) => {
+  let stackString = "";
+  card.cardStack.forEach(
+    (stack) =>
+      (stackString = `${stackString}<li class="border p-1">${stack}</li>`)
+  );
+  cardModal.innerHTML += `
+  <div class="modal fade" id="card-${card.cardId}" tabindex="-1" aria-labelledby="card-${card.id}ModalLabel" aria-hidden="true">
+    <div class="modal-dialog d-flex">
+      <div class="modal-content align-self-lg-center">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="mainProjectModalLabel">${card.cardTitle}</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="main-project d-flex flex-column gap-3 gap-md-0">
+            <ul class="d-flex gap-2 p-0 fs-md-6">
+            ${stackString}
+            </ul>
+            <div class="modal-details-container d-flex flex-column flex-lg-row gap-lg-3">
+              <img src="${card.cardImage}" alt="${card.cardTitle}">
+  
+              <div class="modal-details d-flex flex-column justify-content-between my-3 my-lg-0">
+                <div class="main-project-details d-flex flex-column align-items-start m-lg-0 gap-lg-3">
+                  <p class="fs-md-5">${card.cardDesc}</p>
+                </div>
+                <div>
+                  <button type="button" class="modal-btn border-0 py-2 px-3 px-sm-4"><a href="${card.liveLink}">See Live</a><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
+                    <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
+                  </svg></button>
+                  <button type="button" class="modal-btn border-0 py-2 px-3 px-sm-4"><a href="${card.projLink}">See Source</a><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                  </svg></button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
 });
